710c9e81e4c72fc285799891b32d79b5b533aa33
Esteban Garcia <estebangarcia2121@gmail.com>
2021-04-17 13:28:39 -0700

    Added database deps

---
+++ database/build.rs
@@ -0,0 +1,10 @@
+extern crate protoc_rust;
+
+fn main() {
+    // protoc_rust::Codegen::new()
+    //     .out_dir("./src/protos")
+    //     .inputs(&["../conn/plane.proto"])
+    //     .include("../conn")
+    //     .run()
+    //     .expect("protoc");
+}
