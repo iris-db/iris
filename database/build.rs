00f4fcc9a91f99fd94a36c45819da6412e394809
Esteban Garcia <estebangarcia2121@gmail.com>
2021-04-20 09:34:11 -0700

    Removed comments

--- database/build.rs
+++ database/build.rs
@@ -1,10 +1,1 @@
-extern crate protoc_rust;
-
-fn main() {
-    // protoc_rust::Codegen::new()
-    //     .out_dir("./src/protos")
-    //     .inputs(&["../conn/plane.proto"])
-    //     .include("../conn")
-    //     .run()
-    //     .expect("protoc");
-}
+fn main() {}
