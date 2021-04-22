b58f7ad521dc63c378f7e77e48a6e35633caac78
Esteban Garcia <estebangarcia2121@gmail.com>
2021-04-22 15:17:03 -0700

    Renamed to CallistoDB

--- database/src/lib/filesystem.rs
+++ database/src/lib/filesystem.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
-pub const DATA_PATH: &str = "affinity/data";
-pub const TEMP_PATH: &str = "affinity/temp";
+pub const DATA_PATH: &str = "callistodb/data";
+pub const TEMP_PATH: &str = "callistodb/temp";
 
 pub const PATHS: &[&str] = &[DATA_PATH, TEMP_PATH];
 
