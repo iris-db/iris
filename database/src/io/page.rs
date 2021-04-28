e3b347d5d70666126a4dfcc6cb561ecbfd566ea3
Esteban Garcia <estebangarcia2121@gmail.com>
2021-04-28 14:43:19 -0700

    Added read_header

--- database/src/io/page.rs
+++ database/src/io/page.rs
@@ -1,9 +1,12 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, Cursor, Write};
+use std::string::FromUtf8Error;
+
 use bson::{Bson, Document};
 
 use crate::lib::bson::{Json, JsonObject};
-use std::fs::{File, Metadata};
-use std::io;
-use std::io::{BufRead, Cursor, Error, Read, Write};
 
 /// The maximum amount of data that is able to fit on a single page.
 ///
@@ -30,8 +33,6 @@ pub enum WriteError {
   /// Io error.
   Io(io::Error),
   /// The data attempting to be written will overflow the page size.
-  ///
-  ///
   PageSizeExceeded(usize),
 }
 
@@ -39,7 +40,12 @@ pub enum WriteError {
 pub enum ReadError {
   /// Io error.
   Io(io::Error),
+  /// Error while trying to deserialize a document.
   CorruptedBsonDocument(bson::de::Error),
+  /// Not a UTF8 header.
+  CorruptedHeader(FromUtf8Error),
+  /// Improper key value formatting.
+  MalformedHeader,
 }
 
 /// Creates a new page.
@@ -71,7 +77,7 @@ pub fn write(file: &mut File, contents: Vec<u8>) -> Result<(), WriteError> {
   Ok(())
 }
 
-/// Reads a page header from a set of bytes.
+/// Reads a page header from a set of bytes. Page headers are stored in a `<PAGE_NAME>.meta` file.
 ///
 /// A page header is a key-value string that contains metadata about the page. Header key-pairs are
 /// separated by a whitespace: `KEY1=VALUE KEY2=VALUE`. Headers will always end with a newline
@@ -80,7 +86,36 @@ pub fn write(file: &mut File, contents: Vec<u8>) -> Result<(), WriteError> {
 /// Each header contains the following key-value pairs.
 /// * `COUNT` - The amount of BSON documents
 /// * `OFFSET` - 1 if data overflows to the next page and 0 if all data fits on the current page.
-pub fn read_header() {}
+pub fn read_header(contents: &[u8]) -> Result<HashMap<String, String>, ReadError> {
+  let mut cursor = Cursor::new(contents);
+  let mut header_bytes = Vec::new();
+
+  match cursor.read_until(b'\n', &mut header_bytes) {
+    Err(e) => return Err(ReadError::Io(e)),
+    _ => {}
+  }
+
+  let header = String::from_utf8(header_bytes);
+  let header = match header {
+    Ok(h) => h,
+    Err(e) => return Err(ReadError::CorruptedHeader(e)),
+  };
+
+  let mut kv_pairs: HashMap<String, String> = HashMap::new();
+
+  let kv_pairs_str = header.split_whitespace();
+  for kv_str in kv_pairs_str {
+    let kv = kv_str.split_once('=');
+    let kv = match kv {
+      Some(kv) => kv,
+      None => return Err(ReadError::MalformedHeader),
+    };
+
+    kv_pairs.insert(kv.0.to_string(), kv.1.to_string());
+  }
+
+  Ok(kv_pairs)
+}
 
 /// Reads the data from a BSON page.
 pub fn read_contents<S>(contents: &[u8], serializable: S) -> Result<Vec<S>, ReadError>
@@ -126,12 +161,48 @@ where
 
 #[cfg(test)]
 mod tests {
-  use super::*;
-  use serde_json::{json, Value};
   use std::convert::TryFrom;
 
+  use serde_json::{json, Value};
+
+  use super::*;
+
+  /// Serializable struct for testing.
+  struct IdWrapper {
+    first_name: String,
+    last_name: String,
+  }
+
+  impl PageSerializable for IdWrapper {
+    fn marshall(&self) -> JsonObject {
+      json!({ "firstName": self.first_name, "lastName": self.last_name })
+        .as_object()
+        .unwrap()
+        .clone()
+    }
+
+    fn unmarshall(&self, o: JsonObject) -> Self {
+      IdWrapper {
+        first_name: o.get("firstName").unwrap().as_str().unwrap().to_string(),
+        last_name: o.get("lastName").unwrap().as_str().unwrap().to_string(),
+      }
+    }
+  }
+
   #[test]
-  fn test_read_data() {
+  fn test_read_header() {
+    let header = b"COUNT=12345 OFFSET=1";
+
+    let kvs = read_header(header)
+      .ok()
+      .expect("Could not read key-value pairs from header");
+
+    assert_eq!(kvs.get("COUNT").unwrap(), "12345");
+    assert_eq!(kvs.get("OFFSET").unwrap(), "1");
+  }
+
+  #[test]
+  fn test_read_contents() {
     let original_a = json!(
       {
         "firstName": "John",
@@ -145,27 +216,6 @@ mod tests {
         "lastName": "Brown"
       }
     );
-
-    struct IdWrapper {
-      first_name: String,
-      last_name: String,
-    }
-
-    impl PageSerializable for IdWrapper {
-      fn marshall(&self) -> JsonObject {
-        json!({ "firstName": self.first_name, "lastName": self.last_name })
-          .as_object()
-          .unwrap()
-          .clone()
-      }
-
-      fn unmarshall(&self, o: JsonObject) -> Self {
-        IdWrapper {
-          first_name: o.get("firstName").unwrap().as_str().unwrap().to_string(),
-          last_name: o.get("lastName").unwrap().as_str().unwrap().to_string(),
-        }
-      }
-    }
 
     /// Converts a JSON object into a document.
     fn to_writer(val: Value) -> Vec<u8> {
