1c742f0f820a88e66547c7f92bbef9a3705395f3
Esteban Garcia <estebangarcia2121@gmail.com>
2021-05-08 12:50:39 -0700

    Added cx_freeze and cross platform get arrow key

---
+++ shell/setup.py
@@ -0,0 +1,3 @@
+from cx_Freeze import setup, Executable
+
+setup(name="iqlsh", version="0.1", description="", executables=[Executable("shell.py")])
--- shell/shell.py
+++ shell/shell.py
@@ -1,3 +1,4 @@
+import os
 import platform
 import sys
 
@@ -10,27 +11,73 @@ getch = GetChar()
 if system == "Linux" or system == "Darwin":
 	def get_key():
 		first_char = getch()
-		if first_char == '\x1b':
+		if first_char == "\x1b":
 			return {"[A": "up", "[B": "down", "[C": "right", "[D": "left"}[getch() + getch()]
 		else:
 			return first_char
 elif system == "Windows":
 	def get_key():
 		first_char = getch()
-		if first_char == b'\xe0':
+		if first_char == b"\xe0":
 			return {"H": "up", "P": "down", "M": "right", "K": "left"}[getch()]
 		else:
 			return first_char
+else:
+	print(f"Could not start shell. Unsupported platform {system}")
+	exit(1)
+
+
+def ctrl_key(key):
+	"""Checks if a key is pressed with along with control"""
+	return ord(key) & 0x1f
 
 
 def main():
-	for i in range(0, 20):
+	prompt = ""
+
+	min_index = len(prompt)
+
+	current_line = prompt
+	index = min_index
+
+	sys.stdout.write(prompt)
+	sys.stdout.flush()
+
+	# sys.stdout.write(u"\u001b[" + str(min_index) + "C")
+	# sys.stdout.flush()
+
+	while True:
 		key = get_key()
 
-		if ord(key) == 127:
-			print("\b \b", end="")
+		if len(key) == 1:
+			code = ord(key)
+
+			if code == ctrl_key('c'):
+				break
+			elif 32 <= code <= 126:
+				current_line = current_line[:index] + chr(code) + current_line[index:]
+				index += 1
+			elif code == 127:
+				current_line = current_line[:index - 1] + current_line[index:]
+				index = max(0, index - 1)
+			elif code in {10, 13}:
+				sys.stdout.write(u"\u001b[1000D")
+				print("\nCONTENTS: ", current_line)
+				current_line = ""
+				index = min_index
 		else:
-			print(key, end="")
+			if key == "left":
+				index = max(min_index, index - 1)
+			elif key == "right":
+				index = min(len(current_line), index + 1)
+
+		sys.stdout.write("\33[2K\r")
+		sys.stdout.write(u"\u001b[0K")
+		sys.stdout.write(prompt + current_line)
+		sys.stdout.write("\r")
+
+		if index > min_index:
+			sys.stdout.write(u"\u001b[" + str(index) + "C")
 
 		sys.stdout.flush()
 
