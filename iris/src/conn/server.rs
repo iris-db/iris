54a20470dc16c886c2a3c6110b6d72668faaf9f2
Esteban Garcia <estebangarcia2121@gmail.com>
2021-05-24 11:42:52 -0700

    Added Graph and Return key validation

--- iris/src/conn/server.rs
+++ iris/src/conn/server.rs
@@ -7,16 +7,14 @@ use rocket::{Config, Request, Response, State};
 use rocket_contrib::json::Json;
 
 use crate::conn::response_builder;
+use crate::conn::response_builder::ResponseFormat;
 use crate::conn::response_builder::ResponseFormat::JSON;
-use crate::conn::response_builder::{ResponseFormat, DEFAULT_RESPONSE_FORMAT};
 use crate::database::graph::Graph;
-use crate::io::logger;
 use crate::io::logger::s_log;
 use crate::io::logger::EventCategory::Network;
 use crate::io::logger::EventSeverity::Info;
-use crate::iql::keyword::Keyword;
+use crate::iql::keyword::DispatchQueryContext;
 use crate::lib::json::{fmt_table, JsonObject};
-use rocket::request::{FromRequest, Outcome};
 use serde_json::Value;
 
 /// An IrisDB server.
@@ -82,8 +80,9 @@ fn graph_query(
 	ctx: State<Mutex<Database>>,
 	rf: ResponseFormat,
 ) -> Response {
-	let body = Value::from(body.into_inner());
 	let mut db = ctx.inner().lock().unwrap();
+
+	let body = Value::from(body.into_inner());
 
 	let query = &body["Query"];
 	if query.is_null() || !query.is_array() {
@@ -93,6 +92,40 @@ fn graph_query(
 		)]);
 		return response_builder::new_response(rf, Status::Ok, res);
 	}
+
+	let graph: Option<&mut Graph>;
+
+	let graph_key = &body["Graph"];
+	if !graph_key.is_null() {
+		if !graph_key.is_string() {
+			let res = fmt_table(&vec![response_builder::json_error_object(
+				"Expected a string for Graph",
+				&JsonObject::new(),
+			)]);
+			return response_builder::new_response(rf, Status::Ok, res);
+		}
+
+		graph = db.graphs().get_mut(graph_key.as_str().unwrap());
+	} else {
+		graph = None;
+	}
+
+	let mut return_stmt = "*";
+
+	let return_stmt_key = &body["Return"];
+	if !return_stmt_key.is_null() {
+		if !return_stmt_key.is_string() {
+			let res = fmt_table(&vec![response_builder::json_error_object(
+				"Expected a string for Return",
+				&JsonObject::new(),
+			)]);
+			return response_builder::new_response(rf, Status::Ok, res);
+		}
+
+		return_stmt = return_stmt_key.as_str().unwrap();
+	}
+
+	let query_ctx = DispatchQueryContext::new(graph, query.as_array().unwrap(), return_stmt);
 
 	response_builder::new_response(JSON, Status::Ok, "{}\n".to_string())
 }
