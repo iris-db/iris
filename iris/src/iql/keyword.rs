c7e887a9eb9a8776085a7280f89322441c402be9
Esteban Garcia <estebangarcia2121@gmail.com>
2021-05-24 11:46:14 -0700

    Renamed DispatchKeywordContext to QueryKeywordContext

--- iris/src/iql/keyword.rs
+++ iris/src/iql/keyword.rs
@@ -1,11 +1,13 @@
 use crate::database::graph::Graph;
 use crate::iql::keywords::Insert;
 use crate::lib::json::JsonObject;
+use serde_json::Value;
 use std::collections::HashMap;
 
 /// Error while attempting to execute an action.
 pub enum KeywordExecutionError {
 	/// Keyword body is missing a JSON key.
+	///
 	/// Example: `Missing key at security.credentials.username`
 	MissingKey(String),
 }
@@ -26,21 +28,36 @@ impl dyn Keyword {
 	}
 }
 
-/// Holds the information for executing keywords.
-pub struct DispatchKeywordContext<'a, 'b> {
+/// Holds the information for a query (multiple keywords in a sequence).
+pub struct DispatchQueryContext<'a, 'b> {
 	/// The graph to execute the keyword on.
 	graph: Option<&'a mut Graph>,
-	/// The JSON object holding the keywords.
-	query_object: &'b JsonObject,
+	/// The JSON array holding the query keywords.
+	query: &'b Vec<Value>,
+	/// The object that must be returned from the query.
+	return_stmt: &'b str,
+}
+
+impl<'a, 'b> DispatchQueryContext<'a, 'b> {
+	pub fn new(graph: Option<&'a mut Graph>, query: &'b Vec<Value>, return_stmt: &'b str) -> Self {
+		Self {
+			graph,
+			query,
+			return_stmt,
+		}
+	}
+
+	pub fn execute_query() {}
 }
 
 /// Dispatches all keyword actions from an object in order.
 ///
 /// Returns the executed keywords.
-pub fn dispatch_keywords(ctx: DispatchKeywordContext) -> HashMap<String, ()> {
-	let DispatchKeywordContext {
+pub fn dispatch_keywords(ctx: DispatchQueryContext) -> HashMap<String, ()> {
+	let DispatchQueryContext {
 		graph,
-		query_object,
+		query,
+		return_stmt,
 	} = ctx;
 
 	HashMap::new()
@@ -54,9 +71,9 @@ mod tests {
 	#[test]
 	pub fn test_dispatch_keywords() -> Result<(), WriteError> {
 		let mut graph = Graph::new("a")?;
-		let ctx = DispatchKeywordContext {
+		let ctx = DispatchQueryContext {
 			graph: Some(&mut graph),
-			query_object: &serde_json::from_str(
+			query: &serde_json::from_str(
 				r#"
 			[
 				{
@@ -69,6 +86,7 @@ mod tests {
 			"#,
 			)
 			.unwrap(),
+			return_stmt: "*",
 		};
 
 		let res = dispatch_keywords(ctx);
---
+++ iris/src/iris.rs
@@ -0,0 +1,62 @@
+#![feature(map_first_last)]
+#![feature(test)]
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+use crate::conn::server::Server;
+use crate::io::filesystem::DatabasePath;
+use crate::io::logger::s_log;
+use crate::io::logger::EventSeverity::Info;
+use crate::io::{filesystem, logger};
+use std::env;
+
+mod conn;
+mod database;
+#[allow(warnings, unused)]
+mod generated;
+mod io;
+mod iql;
+mod lib;
+#[allow(unused_imports)]
+mod test_setup;
+use rand::Rng;
+
+use crate::io::logger::EventCategory::{ConnPool, Filesystem, General};
+use serde_json::json;
+
+fn main() {
+	s_log(Info, General, "Starting IrisDB v0.0.1");
+
+	filesystem::prepare();
+
+	for dir in DatabasePath::paths() {
+		s_log(
+			Info,
+			Filesystem,
+			&*format!(
+				"[Directory-InUse] {}/{}",
+				env::current_dir().unwrap().to_str().unwrap(),
+				dir.path()
+			),
+		);
+	}
+
+	s_log(
+		Info,
+		ConnPool,
+		&*format!(
+			"[Connection-Info] {}",
+			json!({
+				"InstanceId": rand::thread_rng().gen_range(0..u16::MAX),
+				"Port": 12712,
+				"ShardCount": 0
+			})
+			.to_string()
+		),
+	);
+
+	let s = Server::new(12712, 0);
+	s.start();
+}
