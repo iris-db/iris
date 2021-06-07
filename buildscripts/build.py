6c446c9ebdaec9f190af805c0fda5f1a888d0dcf
Esteban Garcia <estebangarcia2121@gmail.com>
2021-06-07 11:57:39 -0700

    Added common message logs

--- buildscripts/build.py
+++ buildscripts/build.py
@@ -1,10 +1,39 @@
 import sys
 
-args = sys.argv[1:]
+from buildstages.stages import BUILD_STAGES
 
 
 def main():
-    pass
+    args = sys.argv[1:]
+
+    if len(args) == 0:
+        _MessageUtils.display_help_msg()
+        return
+
+    target = args[0]
+    flags = args[1:]
+
+    try:
+        build_stage = BUILD_STAGES[target]
+        build_stage.run(flags)
+    except KeyError:
+        _MessageUtils.display_invalid_target_error()
+        return
+
+
+class _MessageUtils:
+    """Utility class for printing common CLI messages."""
+
+    def __init__(self):
+        raise TypeError("Cannot create an instance of MessageUtils")
+
+    @staticmethod
+    def display_help_msg():
+        print("Help!")
+
+    @staticmethod
+    def display_invalid_target_error():
+        print("Invalid target.")
 
 
 if __name__ == "__main__":
