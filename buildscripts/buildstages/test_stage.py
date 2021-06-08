355fa9829fedce25059195aa90cf9888c5ef8a8e
Esteban Garcia <estebangarcia2121@gmail.com>
2021-06-07 22:37:30 -0700

    Prefixed test files with test_

---
+++ buildscripts/buildstages/test_stage.py
@@ -0,0 +1,29 @@
+import unittest
+
+from buildstages.stage import FlagSet
+
+
+class FlagSetTest(unittest.TestCase):
+    def test_get(self):
+        alias_first = FlagSet({
+            "-f": "alias.txt",
+            "--file": "long.txt"
+        })
+        self.assertEqual("alias.txt", alias_first.get("--file", "-f"))
+
+        only_alias = FlagSet({
+            "-f": "alias.txt",
+        })
+        self.assertEqual("alias.txt", only_alias.get("--file", "-f"))
+
+        only_long = FlagSet({
+            "--file": "long.txt",
+        })
+        self.assertEqual("long.txt", only_long.get("--file", "-f"))
+
+        no_flags = FlagSet({})
+        self.assertEqual(None, no_flags.get("--file", "-f"))
+
+
+if __name__ == "__main__":
+    unittest.main()
