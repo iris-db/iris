055ba31eca5cd86d8efe37fd58fc4866f3b95f4b
Esteban Garcia <estebangarcia2121@gmail.com>
2021-07-15 22:02:06 -0700

    Added CollectionName
    CollectionName is a utility struct for formatting the name string into a
    file name.

--- source/server/src/storage_engines/orion/collection.rs
+++ source/server/src/storage_engines/orion/collection.rs
@@ -1,17 +1,49 @@
+use crate::page::page::Page;
 use crate::storage_engines::orion::document::Document;
+use std::borrow::Borrow;
 
-pub struct Collection {
-    documents: Vec<Document>,
-}
+pub struct CollectionName(String);
 
-impl Collection {
-    pub fn new() -> Self {
-        Collection {
-            documents: load_documents(32),
-        }
+impl From<String> for CollectionName {
+    fn from(s: String) -> Self {
+        CollectionName(s)
     }
 }
 
-fn load_documents(max_memory: u32) -> Vec<Document> {
-    Vec::new()
+impl CollectionName {
+    /// Get the raw collection name.
+    pub fn name(&self) -> &String {
+        &self.0
+    }
+
+    /// Convert to the raw collection name.
+    pub fn into_name(self) -> String {
+        self.0
+    }
+
+    /// Get the page file name based on the page id.
+    pub fn into_file_name(self, page_id: u32) -> String {
+        format!("{}.{}", self.0, page_id)
+    }
+}
+
+/// An abstraction over data pages.
+pub struct Collection<'a> {
+    name: CollectionName,
+    page: Option<&'a Page<'a>>,
+}
+
+impl Collection<'_> {
+    pub fn new(name: CollectionName) -> Self {
+        let mut col = Collection { name, page: None };
+        col.page = Some(&Page::new(&col).into_ok());
+
+        col
+    }
+
+    pub fn name(&self) -> &CollectionName {
+        &self.name
+    }
+
+    pub fn dispatch_query(&self) {}
 }
